adeeccb0d93b57de28a847d357276aa6ff0af548
Nikolay Lysenko <n-lysenko@yandex-team.ru>
2017-11-13 21:21:49 +0300

    ci: List all currently available tags automatically

---
+++ hooks/post_commit_hook.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# Be sure that a copy of this file is placed to `../.git/hooks` directory
+# under name `post-commit` (without extension).
+
+
+"""
+This script adds to the current commit a refreshed version of
+the list of tags.
+The script is called after every commit automatically if its copy
+is placed and named correctly.
+
+@author: Nikolay Lysenko
+"""
+
+
+import os
+import subprocess
+
+
+def main():
+    # Pre-commit hook has added file to the next commit.
+    # Add it to the current one.
+
+    # Break infinite recursion with disabling post-commit hooks.
+    subprocess.run(
+        '[ -x {file} ] && chmod -x {file}'.format(file=__file__),
+        shell=True
+    )
+
+    relative_path = '../../'
+    script_directory = os.path.dirname(__file__)
+    absolute_path = os.path.join(script_directory, relative_path)
+    subprocess.run(
+        'git commit --amend',
+        cwd=absolute_path,
+        shell=True
+    )
+
+    # Enable post-commit hooks again.
+    subprocess.run(
+        'chmod +x {file}'.format(file=__file__),
+        shell=True
+    )
+
+
+if __name__ == '__main__':
+    main()
---
+++ hooks/pre_commit_hook.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# Be sure that a copy of this file is placed to `../.git/hooks` directory
+# under name `pre-commit` (without extension).
+
+
+"""
+This script updates list of tags that appeared at least once in the
+master notebook with all snippets.
+The script is called during every commit automatically if its copy
+is placed and named correctly.
+
+@author: Nikolay Lysenko
+"""
+
+
+import os
+import subprocess
+import shlex
+import json
+from typing import List
+
+
+def convert_to_absolute_path(relative_path: str) -> str:
+    """
+    Convert relative path to absolute path.
+    """
+    script_directory = os.path.dirname(__file__)
+    absolute_path = os.path.join(script_directory, relative_path)
+    return absolute_path
+
+
+def parse_tags() -> List[str]:
+    """
+    Parse JSON file with notebooks and extract all unique tags.
+    """
+    relative_path = '../../snippets/all_snippets_without_sorting.ipynb'
+    absolute_path = convert_to_absolute_path(relative_path)
+    cells = json.load(open(absolute_path))['cells']
+    tags = set()
+    for cell in cells:
+        tags.update(set(cell['metadata']['tags']))
+    return list(tags)
+
+
+def write_tags(list_of_tags: List[str]) -> type(None):
+    """
+    Override previous content of the file with tags from the list.
+    """
+    relative_path = '../../list_of_tags.txt'
+    absolute_path = convert_to_absolute_path(relative_path)
+    with open(absolute_path, 'w') as out_file:
+        for tag in list_of_tags:
+            out_file.write(tag + '\n')
+
+
+def add_file_with_tags_to_commit() -> type(None):
+    """
+    Add file with tags to the next commit (then post-commit hook
+    will commit it and transfer to the current commit before push).
+    """
+     # `git add ..` can not be run from `.git/*`,
+     # because this directory is outside of work tree.
+    relative_path = '../../'
+    absolute_path = convert_to_absolute_path(relative_path)
+    path_to_add = 'list_of_tags.txt'
+    subprocess.run(
+        'git add {}'.format(path_to_add),
+        cwd=absolute_path,  # Run above command from top level of the repo.
+        shell=True
+    )
+
+
+def main():
+    list_of_tags = parse_tags()
+    write_tags(list_of_tags)
+    add_file_with_tags_to_commit()
+
+
+if __name__ == '__main__':
+    main()
