05e1bc3a9650ec8bab1e26928aefa9d758114a26
Nikolay Lysenko <nikolay.lysenko.1992@gmail.com>
2024-02-18 18:49:41 +0300

    feat: Support multiple domains in Flask app

---
+++ readingbricks/constants.py
@@ -0,0 +1,12 @@
+"""
+Store global constants.
+
+Author: Nikolay Lysenko
+"""
+
+
+DOMAINS = ["machine_learning"]
+DOMAIN_TO_TRANSLATION = {"machine_learning": "Машинное обучение"}
+DOMAIN_TO_SEARCH_PROMPT = {
+    "machine_learning": "нейронные_сети AND (байесовские_методы OR методы_оптимизации)"
+}
